3a04481b84c63d67dc34b0c6ee82fbbc029f7b40
John Wang <johnwangfortune@gmail.com>
2017-06-07 15:48:10 +0400

    first commit

---
+++ UDPPacketListener/stdafx.h
@@ -0,0 +1,24 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#define _WINSOCK_DEPRECATED_NO_WARNIGS
+#define _CRT_SECURE_NO_DEPRECATE
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+#include <cstring>
+#include <algorithm>
+#include <stdint.h>
+#include <exception>
+#include <cstddef>
+#include <vector>
+
+
+
+// TODO: reference additional headers your program requires here
